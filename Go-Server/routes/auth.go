834d8b53592225422b094230cfd4ff98d23bc7cd
Matthew Cania <matthewgab24@gmail.com>
2026-03-15 20:49:01 +0800

    Added ShadCN UI Library

--- Go-Server/routes/auth.go
+++ Go-Server/routes/auth.go
@@ -14,6 +14,7 @@ type SignUpRequest struct {
 	ConfirmPassword string `json:"confirm_password"`
 }
 
+// Won't be used (yet)
 type GoogleSignUpRequest struct {
 	ClientId string `json:"client_id"`
 }
@@ -23,25 +24,29 @@ type SignInRequest struct {
 	Password string `json:"password"`
 }
 
+// Won't be used (yet)
 type GoogleSignInRequest struct {
 	ClientId string `json:"client_id"`
 }
 
 // "/auth"
+const route = "/auth"
+
 func AuthRoute(r *gin.Engine) {
-	r.GET("/auth", func(c *gin.Context) {
+	r.GET(route, func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "auth is running",
 		})
 	})
 
-	r.POST("/auth/signin", func(c *gin.Context) {
+	r.POST(route+"/signin", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "auth is running",
 		})
 	})
 
-	r.POST("/auth/google-signin", func(c *gin.Context) {
+	// Won't be used (yet)
+	r.POST(route+"/google-signin", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "auth is running",
 		})
@@ -53,6 +58,7 @@ func AuthRoute(r *gin.Engine) {
 		})
 	})
 
+	// Won't be used (yet)
 	r.POST("/auth/google-signup", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "auth is running",
---
+++ Go-Server/services/auth.go
@@ -0,0 +1,9 @@
+package services
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func AuthServices(r *gin.Context) {
+
+}
